e41a7bcd80fb6245ea6064960a154b3f8839276b
Kunal <kdshah6593@gmail.com>
2021-08-16 11:23:10 -0400

    completed solution, all tests passing

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+def largest_prime_factor(input)
+    max = 1
+    i = 2
+    total = input
+
+    until total <= 1 || total == max
+        if total % i === 0
+            total = total / i
+            max = i
+        end
+        i += 1
+    end
+
+    max
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,25 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+
+    attr_accessor :input
+
+    def initialize(input)
+        self.input = input
+    end
+
+    def number
+        max = 1
+        i = 2
+        total = self.input
+    
+        until total <= 1 || total == max
+            if total % i === 0
+                total = total / i
+                max = i
+            end
+            i += 1
+        end
+    
+        max
+    end
+end
